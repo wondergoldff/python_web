2ca6cf011258b22390322f5ed1e1571d54e46758
wondergoldff <wondergoldff@gmail.com>
2017-02-15 09:50:40 +0800

    first commit

---
+++ helloTornado.py
@@ -0,0 +1,23 @@
+﻿#!/usr/bin/env python  
+# -*- coding:utf-8 -*-  
+#  
+#   Authhor :Eric.Tang  
+#   Email   :jeepxiaozi66@gmail.com  
+#   Date    :13/06/02 22:17:57  
+#   Desc    :hello,world of tornado  
+#  
+  
+import tornado.ioloop  
+import tornado.web  
+  
+class MainHandler(tornado.web.RequestHandler):  
+    def get(self):  
+        self.write("Hello, world")  
+  
+application = tornado.web.Application([  
+    (r"/",MainHandler),  
+])  
+  
+if __name__=="__main__":  
+    application.listen(8888)  
+    tornado.ioloop.IOLoop.instance().start()   
